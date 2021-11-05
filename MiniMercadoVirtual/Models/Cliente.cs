5845507d7e5d3278f21ea9614810f6e197487ef1
Bruno Vernay Hida <brunovh.df@gmail.com>
2021-11-05 17:05:54 -0300

    Adicionar arquivos de projeto.

---
+++ MiniMercadoVirtual/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MiniMercadoVirtual.Models.Enums;
+
+namespace MiniMercadoVirtual.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Endereco Endereco { get; set; }
+        public DateTime DtInclusao { get; set; }
+        public DateTime DtAlteracao { get; set; }
+        public  StatusCliente Status { get; set; }
+    }
+}
